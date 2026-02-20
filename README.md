4b84be08009e3173299103b595b54d6f4128be05
beatrizfwho <beatrizfhwo@gmail.com>
2026-02-19 21:54:28 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 implentação do tabnews para o curso.dev
--- pages/index.js
+++ pages/index.js
@@ -2,4 +2,8 @@ function Home() {
   return <h1>to live for the hope of it all</h1>;
 }
 
+function teste() {
+  console.log("teste");
+}
+
 export default Home;
